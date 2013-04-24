f750036b65e6d797eccbce756b0b20ae249e3a40
Calum Halcrow <calumhalcrow@gmail.com>
2013-04-24 20:59:54 +0800

    Adds mongoid.
    (What am I letting myself in for!)

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module TripPlanner
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    Mongoid.logger.level = Logger::DEBUG
+    Moped.logger.level = Logger::DEBUG
   end
 end
