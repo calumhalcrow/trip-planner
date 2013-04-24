87c4341e0f266c20637eb6866e1e50d726d4d90f
Calum Halcrow <calumhalcrow@gmail.com>
2013-04-24 20:52:26 +0800

    Base Rails app without ActiveRecord.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TripPlanner::Application.config.secret_token = '41a5cda584dd8470cf10a2c8ac955ab391985d06914560bee291515d7bd9f34e8a1f8225aab67b7e130f23b31fe62d5fb19267f80f69bf3caaa960b3463f69b4'
