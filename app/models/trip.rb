942eeb6aa35e0dc76fc70f4a06a6ec91a0d44378
Calum Halcrow <calumhalcrow@gmail.com>
2013-04-28 15:47:43 +0800

    Spec for Trip model.

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,7 +3,10 @@ class Trip
   include Mongoid::Timestamps
 
   field :name, type: String
+
   has_one :destination, class_name: "Location::Destination"
   has_many :connections
   belongs_to :user
+
+  validates :user, :presence => true
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
 describe Trip do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'allows a valid trip to be created' do
+    user = User.create(:name => 'Mr T', :email => 'mr-t@a-team.tld')
+    Location::Home.create(:name => 'Home', :user => user)
+
+    trip = Trip.create(:name => 'Cherating Getaway', :user => user)
+
+    trip.persisted?.should be_true
+    trip.valid?.should be_true
+
+    Location::Destination.create(:name => 'Cherating', :trip => trip)
+
+    trip.destination.persisted?.should be_true
+
+    connection = Connection.create(
+      :name => 'Home to KLIA',
+      :depart_at => Time.utc(2013,"may",15,8,0,0),
+      :arrive_at => Time.utc(2013,"may",15,13,0,0),
+      :from => user.home,
+      :to => trip.destination,
+      :trip => trip,
+    )
+
+    trip.connections[0].persisted?.should be_true
+    trip.connections[0].name.should eq 'Home to KLIA'
+    trip.connections[0].from.id.should eq user.home.id
+  end
 end
