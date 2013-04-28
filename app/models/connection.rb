4cb6a7bf4dd6549c568f7643ad7d5b6f197e194e
Calum Halcrow <calumhalcrow@gmail.com>
2013-04-28 15:40:55 +0800

    These fields are DateTimes.

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,8 +1,9 @@
 class Connection
   include Mongoid::Document
   include Mongoid::Timestamps
-  field :depart_at, type: Time
-  field :arrive_at, type: Time
+
+  field :depart_at, type: DateTime
+  field :arrive_at, type: DateTime
   field :name, type: String
   has_one :from, class_name: "Location"
   has_one :to, class_name: "Location"
