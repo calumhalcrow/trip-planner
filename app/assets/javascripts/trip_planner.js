a33ea82707c0e3e1ec4cbb319241798b5d4ea84c
Calum Halcrow <calumhalcrow@gmail.com>
2013-05-20 19:30:30 +0800

    Data model over on the client side.

--- app/assets/javascripts/trip_planner.js
+++ app/assets/javascripts/trip_planner.js
@@ -20,10 +20,34 @@ TP.Store = DS.Store.extend({
 TP.User = DS.Model.extend({
   name: DS.attr('string'),
   email: DS.attr('string'),
+  trips: DS.hasMany('TP.Trip'),
+  home: DS.belongsTo('TP.Home')
+});
 
-  allTheThings: function() {
-    return this.get('name') + ' ' + this.get('email');
-  }.property('name', 'email');
+TP.Trip = DS.Model.extend({
+  name: DS.attr('string'),
+  destination: DS.belongsTo('TP.Destination'),
+  connections: DS.hasMany('TP.Connection'),
+  user: DS.belongsTo('TP.User')
+});
+
+TP.Location = DS.Model.extend({
+  name: DS.attr('string'),
+  connections: DS.hasMany('TP.Connection')
+});
+TP.Destination = TP.Location.extend({
+  trip: DS.belongsTo('TP.Trip')
+});
+TP.Home = TP.Location.extend({
+  user: DS.belongsTo('TP.User')
+});
+
+TP.Connection = DS.Model.extend({
+  name: DS.attr('string'),
+  depart_at: DS.attr('date'),
+  arrive_at: DS.attr('date'),
+  location: DS.belongsTo('TP.Location'),
+  trip: DS.belongsTo('TP.Trip')
 });
 
 
