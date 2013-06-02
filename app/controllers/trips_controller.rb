4971344413582c600f7566e9fee6fb24a60528b9
Calum Halcrow <calumhalcrow@gmail.com>
2013-06-02 19:17:20 +0800

    Full response for GET trips/id.

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -6,4 +6,18 @@ class TripsController < RemRestController
     @model = Trip
   end
 
+  def show
+    trip = find_resource
+
+    show = {
+      @resource => {
+        :name     => trip.name,
+        :destination_id => trip.destination._id,
+        :connection_ids => trip.connections.map { |conn| conn._id },
+        :user_id => trip.user._id,
+      }
+    }
+    respond_with show
+  end
+
 end
