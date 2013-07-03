46e485167eed5595510b5051b082a29ecc36a085
Calum Halcrow <calumhalcrow@gmail.com>
2013-07-03 21:09:43 +0800

    Working /users[/:id] request specs.

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,18 @@
 require 'spec_helper'
+require 'factory_girl_rails'
 
 describe "Users" do
   describe "GET /users" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get users_path
+    it "Gets all users." do
+      get users_path, format: :json
+      response.status.should be(200)
+    end
+  end
+
+  describe "GET /users/:id" do
+    it "Get a single user by id." do
+      home = FactoryGirl.create(:home)
+      get "/users/#{home.user.id}", format: :json
       response.status.should be(200)
     end
   end
