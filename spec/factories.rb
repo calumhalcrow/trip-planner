0bfeb02caeb8cc50d4b6b668e71d85bb89790271
Calum Halcrow <calumhalcrow@gmail.com>
2013-07-07 16:57:28 +0800

    Factories updated to new data model.

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,34 +2,36 @@ require 'factory_girl_rails'
 
 FactoryGirl.define do
 
-  factory :connection do
-    depart_at 3.days.ago
-    arrive_at 2.days.ago
-    name "Home to airport"
-    association :location
-  end
-
-  factory :location do
-    name "E&O Georgetown"
-  end
-
-  factory :destination, class: 'Location::Destination' do
-    name "Penang"
-  end
-
-  factory :home, class: 'Location::Home' do
-    name "Home"
-    user
-  end
-
-  factory :trip do
-    name "Penang Summer Getaway"
-    association :user
-    association :destination
-  end
-
   factory :user do
     name "John Doe"
     email  "johndoe@example.com"
   end
+
+  factory :trip do
+    name "Penang Summer Getaway"
+    user
+  end
+
+  factory :destination do
+    name "Penang"
+    trip
+  end
+
+  factory :connection do
+    depart_at 3.days.ago
+    arrive_at 2.days.ago
+    name "Home to airport"
+    trip
+  end
+
+  factory :stop do
+    name "E&O Georgetown"
+    connection
+  end
+
+  factory :home do
+    name "Where the heart is"
+    user
+  end
+
 end
